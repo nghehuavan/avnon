607d65ef975eb3ca4d1c56fe4ba6bdc1cddad3ea
nghe.huavan <nghe.huavan@gmail.com>
2021-05-29 12:17:07 +0700

    ,

---
+++ 53.d94d00547e2b6649a351.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[53],{CEMh:function(n,e,r){"use strict";r.d(e,"a",function(){return s});var t=r("fXoL");let s=(()=>{class n{transform(n,e){return n?n.length<=e?n:n.substr(0,e)+"...":""}}return n.\u0275fac=function(e){return new(e||n)},n.\u0275pipe=t["\u0275\u0275definePipe"]({name:"ellipsis",type:n,pure:!0}),n})()}}]);
